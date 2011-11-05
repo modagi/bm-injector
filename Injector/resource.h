6b79166f249a967b0f6350b70a8cbb5567f42e89
modagi <modagi@88c97760-25b4-3b8d-8a77-1151171e89bb>
2011-11-05 01:50:54 +0000

    [Issue ID]: 2 [Status]: In progress [Title]: supporting 64bit process [Desc]: Just Added controls and variables. Static, Combo and Edit.

--- Injector/resource.h
+++ Injector/resource.h
@@ -23,14 +23,17 @@
 #define IDC_LIST_CODE_RESULT            1022
 #define IDC_STATIC_VERSION              1023
 #define IDC_STATIC_LICENSE              1024
+#define IDC_EDIT_LOG                    1025
+#define IDC_COMBO1                      1026
+#define IDC_COMBO_LOGLEVEL              1026
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_RESOURCE_VALUE        134
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1025
+#define _APS_NEXT_CONTROL_VALUE         1027
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
